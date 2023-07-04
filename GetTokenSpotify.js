355b98db5d91fe9054fc972b1663244a2fa80b1f
Alejandro Camacho Olivares <alejandro.camacho.olivares@gmail.com>
2023-07-04 17:31:20 -0600

    initial commit

---
+++ GetTokenSpotify.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+async function getTokenSpotify() {
+    const URL = "https://accounts.spotify.com/api/token"
+    return fetch(URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": `Basic ${new Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`).toString("base64")}`
+        },
+        body: "grant_type=client_credentials",
+        json: true
+    })
+    .then(data => data.json())
+    .then(response => response.access_token)
+}
+module.exports = getTokenSpotify
