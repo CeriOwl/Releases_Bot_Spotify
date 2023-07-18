472e8217401e17ac842647cc08b61e2d2e299e05
Alejandro Camacho Olivares <alejandro.camacho.olivares@gmail.com>
2023-07-17 18:03:40 -0600

    Root with a message

--- ConnectionHost.js
+++ ConnectionHost.js
@@ -3,6 +3,9 @@ const app = express()
 const PORT = process.env.PORT || 4000
 
 const activateServer = () => {
+    app.get("/", (req, res) => {
+        res.send("Hello World")
+    })
     app.listen(PORT, () => {
         console.log("Listening in port: " + PORT)
     })
